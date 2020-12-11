9de2e07fc322f8a1498166f90035c6e10b23e766
Maarten Dirkse <maarten.dirkse@gmail.com>
2020-12-11 13:13:24 +0100

    Prevent ssh login with password

--- modules/octopi/manifests/init.pp
+++ modules/octopi/manifests/init.pp
@@ -1,6 +1,20 @@
 class octopi {
-  service{ 'bluetooth':
+  service{ ['avahi-daemon', 'alsa-restore', 'alsa-state','bluetooth']:
     ensure => stopped,
     enable => false,
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure => 'file',
+    owner  => 0,
+    group  => 0,
+    mode   => '0644',
+    source => 'puppet:///modules/octopi/sshd_config',
+    notify => Service['ssh'],
+  }
+
+  service{ 'ssh':
+    ensure => running,
+    enable => true,
   }
 }
